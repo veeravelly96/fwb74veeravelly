3adf483099cc96e645600bc55eb85d67c9e7f33e
sai krishna veeravelly <S544724@NWMISSOURI.EDU>
2021-11-02 00:19:46 -0500

    added mango

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mango', { title: 'Search results mango' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
