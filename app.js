508f177ef881314165f4bb518158c67122d676d8
sai krishna veeravelly <S544724@NWMISSOURI.EDU>
2021-11-02 21:51:53 -0500

    bonus

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mangoRouter = require('./routes/mango');
-
+var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 var app = express();
 
 // view engine setup
@@ -23,7 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mango', mangoRouter);
-
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
